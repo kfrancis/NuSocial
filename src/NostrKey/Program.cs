b61c4440544d4417c5cdb200428b0412a8b69134
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-01-03 16:48:06 -0500

    Mine for difficulty

--- src/NostrKey/Program.cs
+++ src/NostrKey/Program.cs
@@ -1,32 +1,17 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Text;
 using NBitcoin;
 using NBitcoin.Secp256k1;
+using SimpleBase;
 using Spectre.Console;
 using Spectre.Console.Cli;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace NostrKey
 {
-    internal class Program
-    {
-        static int Main(string[] args)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                // this makes emojis come up more reliably. might get built into spectre better in the future, so give a go deleting this at some point
-                // they seem to show up fine on osx and actually need this to be off to work there
-                Console.OutputEncoding = new System.Text.UTF8Encoding();
-            }
-
-            var app = new CommandApp<GenerateNostrKeyCommand>();
-            return app.Run(args);
-        }
-    }
-
     internal static class Extensions
     {
         public static string ToHex(this byte[] bytes)
@@ -65,26 +50,10 @@ namespace NostrKey
 
     internal sealed class GenerateNostrKeyCommand : Command<GenerateNostrKeyCommand.Settings>
     {
-        private static Scalar GenerateScalar()
-        {
-            var scalar = Scalar.Zero;
-            Span<byte> output = stackalloc byte[32];
-            do
-            {
-                RandomUtils.GetBytes(output);
-                scalar = new Scalar(output, out var overflow);
-                if (overflow != 0 || scalar.IsZero)
-                {
-                    continue;
-                }
-                break;
-            } while (true);
-            return scalar;
-        }
+        private readonly object _obj = new();
 
         public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
         {
-            int numCores = GetCores();
             var powDifficulty = settings.Difficulty;
 
             if (!string.IsNullOrEmpty(settings.VanityPrefix) || !string.IsNullOrEmpty(settings.VanityPubPrefix))
@@ -111,6 +80,7 @@ namespace NostrKey
                 AnsiConsole.WriteLine($"Started mining process with a difficulty of: {powDifficulty}");
             }
 
+            var numCores = Environment.ProcessorCount;
             if (!string.IsNullOrEmpty(settings.VanityPubPrefix))
             {
                 // Benchmarking of cores disabled for vanity npub key upon proper calculation.
@@ -120,44 +90,122 @@ namespace NostrKey
                 BenchmarkCores(numCores, powDifficulty);
             }
 
-            var key = GenerateScalar();
-
-            ECPrivKey? privKey = null;
-
-            try
+            if (powDifficulty > 0)
             {
-                if (Context.Instance.TryCreateECPrivKey(key, out privKey))
+                var iterationCount = 0L;
+                var workerThread = (long bestDiff, string prefix, string pubPrefix) =>
                 {
-                    var pubKey = privKey.CreateXOnlyPubKey();
-                    Span<byte> privKeyc = stackalloc byte[65];
-                    privKey.WriteToSpan(privKeyc);
-                    var pubKeyStr = pubKey.ToBytes().ToHex();
-                    var privKeyStr = privKeyc.ToHex()[..64];
+                    var is_valid_pubkey = false;
+                    var now = DateTime.Now;
+                    while (!is_valid_pubkey)
+                    {
+                        Interlocked.Increment(ref iterationCount);
 
-                    AnsiConsole.MarkupLine($"Public Key: [green]{pubKeyStr}[/]");
-                    AnsiConsole.MarkupLine($"Private Key: [green]{privKeyStr}[/]");
-                }
-                else
+                        // something
+                        var rng = GenerateScalar();
+                        var (privateKey, publicKey) = GenerateKeyPair(rng);
+                        var leading_zeroes = 0;
+
+                        if (!string.IsNullOrEmpty(prefix) || !string.IsNullOrEmpty(pubPrefix))
+                        {
+                            var hexa_key = publicKey.ToBytes().ToHex();
+                            var is_valid_pubkey_hex = true;
+                            var is_valid_pubkey_bech32 = true;
+
+                            if (!string.IsNullOrEmpty(prefix))
+                            {
+                                is_valid_pubkey_hex = hexa_key.StartsWith(prefix, StringComparison.Ordinal);
+                            }
+
+                            if (!string.IsNullOrEmpty(pubPrefix))
+                            {
+                                var prefixBytes = Encoding.ASCII.GetBytes(pubPrefix + hexa_key);
+                                var bechKey = Base32.Bech32.Encode(prefixBytes);
+                                is_valid_pubkey_bech32 = bechKey.StartsWith("npub1" + pubPrefix, StringComparison.Ordinal);
+                            }
+
+                            // only valid if both options are valid
+                            // it one of both were not required, then it's considered valid
+                            is_valid_pubkey = is_valid_pubkey_hex && is_valid_pubkey_bech32;
+                        }
+                        else
+                        {
+                            leading_zeroes = GetLeadingZeroBits(publicKey);
+                            is_valid_pubkey = leading_zeroes > powDifficulty;
+                            if (is_valid_pubkey)
+                            {
+                                AnsiConsole.WriteLine($"Leading zero bits: {leading_zeroes}");
+                            }
+                        }
+
+                        if (is_valid_pubkey)
+                        {
+                            var iterations = Interlocked.Read(ref iterationCount);
+                            var iterString = $"{iterations}";
+                            var l = iterString.Length;
+                            var f = iterString[0];
+
+                            PrintKeys(privateKey, publicKey, iterations, f, l, now);
+
+                            if (Interlocked.Read(ref bestDiff) > 0)
+                            {
+                                Interlocked.Exchange(ref bestDiff, leading_zeroes);
+                            }
+                        }
+                    }
+                };
+
+                var threadCount = Math.Max(1, Math.Min(numCores, Math.Min(8, 8)));
+                var threads = new Task[threadCount - 1];
+                for (var i = 0; i < threads.Length; i++)
                 {
-                    throw new NotImplementedException();
+                    threads[i] = Task.Run(() => workerThread(powDifficulty, settings.VanityPrefix, settings.VanityPubPrefix));
                 }
+                AnsiConsole.Status().Start("Thinking...", ctx =>
+                {
+                    workerThread(powDifficulty, settings.VanityPrefix, settings.VanityPubPrefix);
+                    for (var i = 0; i < threads.Length; i++)
+                    {
+                        threads[i].Wait();
+                    }
+                });
             }
-            finally
+            else
             {
-                privKey?.Dispose();
+                var key = GenerateScalar();
+
+                ECPrivKey? privKey = null;
+
+                try
+                {
+                    if (Context.Instance.TryCreateECPrivKey(key, out privKey))
+                    {
+                        var pubKey = privKey.CreateXOnlyPubKey();
+
+                        PrintKeys(privKey, pubKey);
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                finally
+                {
+                    privKey?.Dispose();
+                }
             }
 
             return 0;
         }
 
-        private void BenchmarkCores(int numCores, int powDifficulty)
+        private static void BenchmarkCores(int numCores, int powDifficulty)
         {
             var hashes_per_second_per_core = 0;
 
             AnsiConsole.WriteLine("Benchmarking a single core for 5 seconds...");
-            Stopwatch sw = new Stopwatch();
-            int msLimit = 5000;
-            bool bDoExit = false;
+            var sw = new Stopwatch();
+            var msLimit = 5000;
+            var bDoExit = false;
             sw.Start();
             while (!bDoExit)
             {
@@ -174,29 +222,124 @@ namespace NostrKey
                 }
             }
 
-            hashes_per_second_per_core /= 10;
+            hashes_per_second_per_core /= numCores;
             AnsiConsole.WriteLine($"A single core can mine roughly {hashes_per_second_per_core} h/s!");
         }
 
-        private int GetCores()
+        private static (ECPrivKey privateKey, ECXOnlyPubKey publicKey) GenerateKeyPair(Scalar rng)
         {
-            return Environment.ProcessorCount;
+            ECPrivKey? privKey = null;
+
+            if (Context.Instance.TryCreateECPrivKey(rng, out privKey))
+            {
+                var pubKey = privKey.CreateXOnlyPubKey();
+
+                return (privKey, pubKey);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private static Scalar GenerateScalar()
+        {
+            var scalar = Scalar.Zero;
+            Span<byte> output = stackalloc byte[32];
+            do
+            {
+                RandomUtils.GetBytes(output);
+                scalar = new Scalar(output, out var overflow);
+                if (overflow != 0 || scalar.IsZero)
+                {
+                    continue;
+                }
+                break;
+            } while (true);
+            return scalar;
+        }
+
+        private static int GetLeadingZeroBits(ECXOnlyPubKey publicKey)
+        {
+            byte res = 0;
+            foreach (var b in publicKey.ToBytes())
+            {
+                if (b == 0)
+                {
+                    res += 8;
+                }
+                else
+                {
+                    res += (byte)BitOperations.LeadingZeroCount(b);
+                    return res;
+                }
+            }
+            return res;
+        }
+
+        private void PrintKeys(ECPrivKey privKey, ECXOnlyPubKey pubKey)
+        {
+            lock (_obj)
+            {
+                Span<byte> privKeyc = stackalloc byte[65];
+                privKey.WriteToSpan(privKeyc);
+                var pubKeyStr = pubKey.ToBytes().ToHex();
+                var privKeyStr = privKeyc.ToHex()[..64];
+
+                AnsiConsole.MarkupLine($"Public Key: [green]{pubKeyStr}[/]");
+                AnsiConsole.MarkupLine($"Private Key: [green]{privKeyStr}[/]");
+            }
+        }
+
+        private void PrintKeys(ECPrivKey privKey, ECXOnlyPubKey pubKey, long iterations, char f, int l, DateTime now)
+        {
+            lock (_obj)
+            {
+                Span<byte> privKeyc = stackalloc byte[65];
+                privKey.WriteToSpan(privKeyc);
+                var pubKeyStr = pubKey.ToBytes().ToHex();
+                var privKeyStr = privKeyc.ToHex()[..64];
+
+                AnsiConsole.MarkupLine($"Public Key: [green]{pubKeyStr}[/]");
+                AnsiConsole.MarkupLine($"Private Key: [green]{privKeyStr}[/]");
+
+                AnsiConsole.WriteLine("{0} iterations (about {1}x10^{2} hashes) in {3} seconds. Avg rate {4} hashes/second",
+                                iterations, f, l - 1, DateTime.Now.Subtract(now).TotalSeconds,
+                                iterations / Math.Max(1, DateTime.Now.Subtract(now).TotalSeconds));
+            }
         }
 
         public sealed class Settings : CommandSettings
         {
             [Description("Difficulty")]
-            [CommandArgument(0, "<difficulty>")]
+            [CommandArgument(0, "[difficulty]")]
+            [DefaultValue(0)]
             public int Difficulty { get; init; }
 
             [Description("Vanity Prefix")]
-            [CommandOption("-v|--vanityPrefix <PREFIX>")]
+            [CommandOption("-p|--vanityPrefix <PREFIX>")]
             [DefaultValue("")]
             public string VanityPrefix { get; init; }
 
             [CommandOption("-c|--vanityPubPrefix <PREFIX>")]
             [DefaultValue("")]
             public string VanityPubPrefix { get; init; }
+        }
+    }
+
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // this makes emojis come up more reliably. might get built into spectre better in the future, so give a go deleting this at some point
+                // they seem to show up fine on osx and actually need this to be off to work there
+                Console.OutputEncoding = new System.Text.UTF8Encoding();
+            }
+
+            var app = new CommandApp<GenerateNostrKeyCommand>();
+            return app.Run(args);
         }
     }
 }
