b9608b497bb95545d7b38b8702a4e26724fd02be
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-05-18 21:03:35 -0400

    my vs mutual relays

--- src/NuSocial/ViewModels/RelaysViewModel.cs
+++ src/NuSocial/ViewModels/RelaysViewModel.cs
@@ -3,24 +3,61 @@ using Volo.Abp.DependencyInjection;
 
 namespace NuSocial.ViewModels;
 
+public partial class RelayViewTypeOption
+{
+    public Color BackgroundColour { get; set; } = Colors.Transparent;
+
+    public string Name { get; set; } = string.Empty;
+    public RelayViewType ViewType { get; set; }
+}
+
 public partial class RelaysViewModel : BaseViewModel, ITransientDependency
 {
+    private readonly IDatabase _db;
+
+    [ObservableProperty]
+    private ObservableCollection<RelayViewTypeOption> _options = new();
+
+    [ObservableProperty]
+    private ObservableCollection<Relay> _relays = new();
+
+    [ObservableProperty]
+    private RelayViewType _viewType = RelayViewType.My;
+
     public RelaysViewModel(IDialogService dialogService,
-                           INavigationService navigationService,
-                           IDatabase db) 
+                                           INavigationService navigationService,
+                           IDatabase db)
         : base(dialogService, navigationService)
     {
         Title = L["Relays"];
         _db = db;
     }
 
-    [ObservableProperty]
-    private ObservableCollection<Relay> _relays = new();
-
-    private readonly IDatabase _db;
-
     public override async Task InitializeAsync()
     {
         Relays = await _db.GetRelaysAsync();
+        SetOptions(ViewType);
+    }
+
+    [RelayCommand(CanExecute = nameof(IsNotBusy))]
+    private Task ChangeViewTypeAsync(RelayViewTypeOption viewType)
+    {
+        return SetBusyAsync(() =>
+        {
+            SetOptions(viewType.ViewType); return Task.CompletedTask;
+        });
+    }
+
+    private void SetOptions(RelayViewType viewType)
+    {
+        Title = viewType switch
+        {
+            RelayViewType.My => L["My"],
+            RelayViewType.Mutual => L["Mutual"],
+            _ => L["Unknown"],
+        };
+
+        Options.Clear();
+        Enum.GetNames<RelayViewType>().ToList().ForEach(x => Options.AddLast(new RelayViewTypeOption() { Name = L[$"Enum_{x}"], ViewType = (RelayViewType)Enum.Parse(typeof(RelayViewType), x) }));
     }
 }
